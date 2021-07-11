11c8a947ca83bbad9c3a04da7f23045e7368b305
hope-redemption <1606817007@qq.com>
2021-07-11 18:56:15 +0800

    add a button

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "demo_git.h"
 
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     demo_git w;
+
+    QPushButton b;//添加按钮对象
+    b.setText("^_^");//给按钮添加内容
+    b.setParent(&w);//方法一
+    b.move(100,100);
+
     w.show();
     return a.exec();
 }
